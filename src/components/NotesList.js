0f49be3ab306f1d2408d4323d204d1862e3c97f1
stylessh <stylesshjs@gmail.com>
2022-05-30 17:17:35 -0400

    feat: now can delete notes

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,24 +1,57 @@
-import React from "react";
-import { Grid, Card, Text } from "@geist-ui/core";
-import { getNotes } from "../utils/notes";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Grid, Card, Text, Button } from "@geist-ui/core";
+import { Trash } from "@geist-ui/icons";
+
+import { useNavigate } from "react-router-dom";
+
+import { deleteNote, getNotes } from "../utils/notes";
 
 const NotesList = () => {
-  // Get notes
-  const notes = getNotes();
+  const navigate = useNavigate();
+  const [notes, setNotes] = useState(() => getNotes());
+
+  const handleDeleteNote = (e, id) => {
+    e.stopPropagation();
+    deleteNote(id);
+
+    const newNotes = getNotes();
+    setNotes(newNotes);
+  };
 
   return (
     <Grid.Container gap={2}>
       {notes.map((note) => (
         <Grid xs={24} sm={12} md={8} lg={6} xl={6} key={note.id}>
-          <Link to={`/note/${note.id}`} style={{ width: "100%" }}>
-            <Card hoverable style={{ cursor: "pointer" }} width="100%">
-              <Text h3 font="1.2em">
-                {note.title}
-              </Text>
-              <p>{note.date}</p>
-            </Card>
-          </Link>
+          <Card
+            hoverable
+            style={{ cursor: "pointer", position: "relative" }}
+            width="100%"
+            className="notes-list-item"
+            onClick={() => navigate("/note/" + note.id)}
+          >
+            <Text h3 font="1.2em">
+              {note.title}
+            </Text>
+            <p>{note.date}</p>
+
+            {/* Delete button */}
+
+            <Button
+              auto
+              scale={0.5}
+              type="error"
+              ghost
+              icon={<Trash />}
+              className="delete-button"
+              style={{
+                position: "absolute",
+                top: "2em",
+                right: "1.5em",
+                zIndex: 10,
+              }}
+              onClick={(e) => handleDeleteNote(e, note.id)}
+            />
+          </Card>
         </Grid>
       ))}
       {/* Fallback */}
